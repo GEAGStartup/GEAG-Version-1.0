1b7299d39949ccda1b516a2fbe4dbe03e854a595
Tiago Silva <tiago.silva.1996@hotmail.com>
2017-05-22 16:48:20 +0100

    alteração de limites a funcionar

--- app/controllers/logged_controller.rb
+++ app/controllers/logged_controller.rb
@@ -1,27 +1,33 @@
 class LoggedController < ApplicationController
   def show
   end
+
   def electricity
     @meter = Meter.find_by(type_id:1, user_id: current_user.id)
     @limite= Limite.find_by(meter_id:(3*current_user.id-2))
   end
+
   def water
     @meter = Meter.find_by(type_id:2, user_id: current_user.id)
     @limite = Limite.find_by(meter_id:(3*current_user.id-1))
   end
+
   def gas
     @meter = Meter.find_by(type_id:3, user_id: current_user.id)
     @limite = Limite.find_by(meter_id:(3*current_user.id))
   end
+
   def settings
 
   end
 
   def create
-    @limite = Limite.find_by(meter_id:(3*current_user.id))#User.find(params[:user_id])
-    @limite.value=(params[:limite.value3])
-    @limite.save#@user.limites.new(limites_params)
-    alert('ola')
+    @limite = Limite.find_by(meter_id:(3*current_user.id-2))
+    @limite.update_attributes(value: params[:limite][:value1])
+    @limite = Limite.find_by(meter_id:(3*current_user.id-1))
+    @limite.update_attributes(value: params[:limite][:value2])
+    @limite = Limite.find_by(meter_id:(3*current_user.id))
+    @limite.update_attributes(value: params[:limite][:value3])
     redirect_to logged_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+  root to: "logged#show"
+  post 'settings' => 'logged#create'
+  get 'logged' , to: "logged#show"
+  get 'electricity' , to: "logged#electricity"
+  get 'water' , to: "logged#water"
+  get 'gas' , to: "logged#gas"
+  get 'settings', to:"logged#settings"
+  delete 'logged', to: "layouts#application"
   resources :meters
   resources :types
   resources :application
   resources :consumptions
   resources :alerts
   resources :limites
-  root to: "logged#show"
-  get 'logged' , to: "logged#show"
-  get 'electricity' , to: "logged#electricity"
-  get 'water' , to: "logged#water"
-  get 'gas' , to: "logged#gas"
-  get 'settings', to:"logged#settings"
-  post 'settings', action:"logged#create", to: "logged#show"
-  delete 'logged', to: "layouts#application"
+
+
 end
